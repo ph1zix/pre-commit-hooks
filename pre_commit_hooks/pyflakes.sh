25562cdeb71fd3f0f601b603790abaf12082a83c
ph1zix <dbforma3000@gmail.com>
2023-08-23 21:13:03 +0200

    install pyflakes

--- pre_commit_hooks/pyflakes.sh
+++ pre_commit_hooks/pyflakes.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "Running pyflakes.sh on working directory.."
+echo "[INFO] Checking if pyflakes is installed.."
+alias python=python3 
+if python -c "import pyflakes" &> /dev/null; then
+    echo "[INFO] Pyflakes is already installed"
+else 
+    echo "[INFO] Will install pyflakes with pip.."
+    pip -m install pyflakes
+fi
+
+echo "[INFO] Running pyflakes.sh on working directory.."
 
 find_cmd="find $PWD -type d -name '.env' -prune -o -type f -name '*.py' -print"
 xargs_cmd="xargs pyflakes"
@@ -10,7 +19,7 @@ stdout_xargs=$(echo "$stdout_find" | eval "$xargs_cmd" 2>&1)
 
 
 if [ $? -ne 0 ]; then
-echo -e "\e[33m[WARN]\e[0m Pyflakes found some violations. Please fix them or force the commit with --no-verify"
-echo "$stdout_xargs"
-exit 1
+    echo -e "\e[33m[WARN]\e[0m Pyflakes found some violations. Please fix them or force the commit with --no-verify"
+    echo "$stdout_xargs"
+    exit 1
 fi
