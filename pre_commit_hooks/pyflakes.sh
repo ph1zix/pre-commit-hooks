e34f21480a6c7d204d96c4472c89f6cfcb869320
ph1zix <dbforma3000@gmail.com>
2023-08-23 21:36:53 +0200

    different approach

--- pre_commit_hooks/pyflakes.sh
+++ pre_commit_hooks/pyflakes.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 echo "[INFO] Checking if pyflakes is installed.."
-alias python=python3 
-python -c 'import pyflakes' 2>/dev/null && echo "python pyflakes modules installed" || echo "python pyflakes modules not installed"
+is_pyflakes_installed="${pip list | grep -F pyflakes}"
+
+if [[ -n $is_pyflakes_installed ]]
+then
+    echo "pyflakes module already installed"
+else
+    pip install pyflakes
+fi
 
 echo "[INFO] Running pyflakes.sh on working directory.."
-
 find_cmd="find $PWD -type d -name '.env' -prune -o -type f -name '*.py' -print"
 xargs_cmd="xargs pyflakes"
 
 stdout_find=$(eval "$find_cmd")
 stdout_xargs=$(echo "$stdout_find" | eval "$xargs_cmd" 2>&1)
-
 
 if [ $? -ne 0 ]; then
     echo -e "\e[33m[WARN]\e[0m Pyflakes found some violations. Please fix them or force the commit with --no-verify"
