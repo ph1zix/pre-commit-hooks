0d7250556525c91fc8a24ce802171cd9fd07aeaf
ph1zix <dbforma3000@gmail.com>
2023-08-23 20:04:07 +0200

    include rest

--- pre_commit_hooks/string_fixer.py
+++ pre_commit_hooks/string_fixer.py
@@ -56,3 +56,25 @@ def fix_strings(filename: str) -> int:
         with open(filename, 'w', encoding='UTF-8', newline='') as f:
             f.write(new_contents)
         return 1
+    else:
+        return 0
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to fix')
+    args = parser.parse_args(argv)
+
+    retv = 0
+
+    for filename in args.filenames:
+        return_value = fix_strings(filename)
+        if return_value != 0:
+            print(f'Fixing strings in {filename}')
+        retv |= return_value
+
+    return retv
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
