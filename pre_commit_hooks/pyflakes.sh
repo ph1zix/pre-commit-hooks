f39e945a3e728bd72e2db20ccaf1fd1d234eeb56
ph1zix <dbforma3000@gmail.com>
2023-08-23 20:39:59 +0200

    some colering

--- pre_commit_hooks/pyflakes.sh
+++ pre_commit_hooks/pyflakes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Running hook-pyflakes.sh..."
+echo "Running pyflakes.sh on working directory.."
 
 find_cmd="find $PWD -type d -name '.env' -prune -o -type f -name '*.py' -print"
 xargs_cmd="xargs pyflakes"
@@ -8,8 +8,11 @@ xargs_cmd="xargs pyflakes"
 stdout_find=$(eval "$find_cmd")
 stdout_xargs=$(echo "$stdout_find" | eval "$xargs_cmd" 2>&1)
 
+WARN='\033[0;33m'
+STD='\033[0m'
+
 if [ $? -ne 0 ]; then
-echo "[INFO] Pyflakes found some violations. Please fix them or force the commit with --no-verify"
-echo "$stdout_xargs"
-exit 1
+    echo "${WARN}[WARN]${STD} Pyflakes found some violations. Please fix them or force the commit with --no-verify"
+    echo "$stdout_xargs"
+    exit 1
 fi
