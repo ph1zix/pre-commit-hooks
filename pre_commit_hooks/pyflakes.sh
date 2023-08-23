57e3c90e8529b9a73cc12dac4ae76f46cfa673f6
ph1zix <dbforma3000@gmail.com>
2023-08-23 20:49:53 +0200

    widcolorz

--- pre_commit_hooks/pyflakes.sh
+++ pre_commit_hooks/pyflakes.sh
@@ -8,11 +8,9 @@ xargs_cmd="xargs pyflakes"
 stdout_find=$(eval "$find_cmd")
 stdout_xargs=$(echo "$stdout_find" | eval "$xargs_cmd" 2>&1)
 
-WARN='\033[0;33m'
-STD='\033[0m'
 
 if [ $? -ne 0 ]; then
-echo "${WARN}[WARN]${STD} Pyflakes found some violations. Please fix them or force the commit with --no-verify"
+echo -e "\e[33m[WARN]\e[0m Pyflakes found some violations. Please fix them or force the commit with --no-verify"
 echo "$stdout_xargs"
 exit 1
 fi
